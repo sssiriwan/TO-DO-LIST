8ca88d3e1fb6f837b30afa5cf19342652590bf3d
sssiriwan <siriwan9933@gmail.com>
2023-09-17 18:46:13 +0700

    feat:function-add-list-and-check-list-remove-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    
+  ]);
+
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    const newTodo = { id: Date.now(), text: inputRef.current.value, checked: false };
+    setTodos([...todos, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const handleRemoveTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleToggleTodo = (id) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !todo.checked;
+      }
+      return todo;
+    }));
+  };
 
   return (
-    
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-     
-  )
+    <div className=" w-full max-w-[540px] bg-white mt-[100px] mx-auto mb-[20px] pt-[40px] pb-[70px] px-[30px] rounded-lg ">
+      <div className=" flex items-center mb-5  justify-center">
+        <h1 className="text-3xl font-bold text-black">TO-DO List</h1>
+        <img src="src/assets/To-Do-Img/icon.png" className=" w-[30px] ml-[10px]  " />
+      </div>
+      <div className=" flex items-center justify-between bg-[#edeef0] rounded-[30px] pl-[20px] mb-[25px] ">
+        <input ref={inputRef} className=" flex-1 border-none outline-none bg-transparent p-[10px] font-medium " type="text" placeholder="Add your text"/>
+        <button onClick={handleAddTodo} className="  border-none outline-none  bg-[#ff5945] text-yellow-50 font-[16px] cursor-pointer rounded-full py-[16px] px-[50px]  ">Add</button>
+      </div>
+      <ul >
+        {todos.map((todo) => (
+          <li key={todo.id} className={todo.checked ? "checked" : ""}>
+            <input type="checkbox" checked={todo.checked} onChange={() => handleToggleTodo(todo.id)} />
+            {todo.text}
+            <button className=" " onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
